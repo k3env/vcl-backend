948ec4d7c72965c4a5aace3784a3c04462ca106b
Sergey Tulonov <me@k3env.site>
2022-07-25 22:45:38 +0800

    Added 2 routes Added Heroku deploy instructions

--- app/Controllers/Http/AuthenticationController.ts
+++ app/Controllers/Http/AuthenticationController.ts
@@ -1,3 +1,26 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
 
-export default class AuthenticationController {}
+export default class AuthenticationController {
+  public async create({ request, response }: HttpContextContract) {
+    console.log()
+    if (User.findBy('email', request.body()['email']) === null) {
+      try {
+        let u = new User()
+        u.email = request.body()['email']
+        u.password = request.body()['password']
+        await u.save()
+        console.log(u.$isPersisted)
+
+        response.send({ status: 200, user: u, rawBody: request.body })
+      } catch (e) {
+        response.gone({ error: e })
+      }
+    } else {
+      response.forbidden({ message: 'Already exists' })
+    }
+  }
+  public async list({ response }: HttpContextContract) {
+    response.send({ status: 200, users: await User.all() })
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,6 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async () => {
   return { hello: 'world' }
 })
+
+Route.post('/api/user', 'AuthenticationController.create')
+Route.get('/api/user', 'AuthenticationController.list')
